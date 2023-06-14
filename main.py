320b49eafda91eb2d1f60f99e75cda989a4449e8
Kristina Polkhutenkova <kristinapolhutenkovatai@gmail.com>
2023-06-15 00:55:32 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#  source
+source_id = 23456
+source_name = 'John'
+source_type = 'Confidential Informant'  #
+source_reliability = 7.09  #
+source_accessibility = True  # true -
+source_contact = ''  # phone number, email, address
+source_description = ''
+source_history = ''
+
+
+#  information
+content = ''
+date_of_information = ''
+location_of_information = ''
+source_reliability
+verification_status = True
+supporting_evidence = ''
