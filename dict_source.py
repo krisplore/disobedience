f1b0d6790fca431176bb27e0e81959236859cef8
Kristina Polkhutenkova <kristinapolhutenkovatai@gmail.com>
2023-06-15 22:08:22 +0700

    second commit pet

--- dict_source.py
+++ dict_source.py
@@ -1,50 +1,80 @@
-import yaml
-import uuid
 import datetime
+import getopt
 import re
+import sys
+import uuid
+import yaml
+import gettext
 
-def set_id():
-    return str(uuid.uuid5(uuid.NAMESPACE_OID, callsign))
+EXTENSION = '.yaml'
+NUMBER_INVITE = 2  # вынести и переименовать
+
+
+def set_id():  #
+    return str(uuid.uuid5(uuid.NAMESPACE_DNS, 'disobedience'))
+
 
 def set_type():
     return 1
 
-def set_invite():
-    NUMBER_INVITE = 2
+
+def set_invite():  #
     invite = []
     for _ in range(NUMBER_INVITE):
-        invite.append(str(uuid.uuid4()))
+        invite.append(str(uuid.uuid4()))  # 6 англ символов и цифр без спорных uppercase
     return invite
 
-def set_tegs(tegs):
-    tegs = tegs.split(',')
-    tegs = [re.sub(r'\s+', ' ', item.strip()) for item in tegs]
-    return tegs
+
+def set_tags(tags):  # tags!
+    tags = tags.split(',')  # а что если запятой нет defensive programming
+    tags = [re.sub(r'\s+', ' ', item.strip()) for item in tags]  # убрать re
+    return tags
 
 
-source = {}
-user_key = ['callsign', 'tegs', 'invited_by']
+def main(argv):
+    callsign = ''
+    tags = []
+    invited_by = ''
+
+    try:
+        opts, args = getopt.getopt(argv, 'c:t:i:', ['callsign=', 'tags=', 'invited_by='])
+    except getopt.GetoptError:  #
+        print('There is something wrong with arguments')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ('-c', '--callsign'):
+            callsign = arg      #
+        elif opt in ('-t', '--tags'):
+            tags = arg
+        elif opt in ('-i', '--invited_by'):
+            invited_by = arg
+
+    source = {}
+    source['callsign'] = callsign
+    source['tags'] = tags
+    source['invited_by'] = invited_by
+
+    source['id'] = set_id()
+    source['type'] = set_type()
+    source['reliability'] = 4.98
+    source['tags'] = set_tags(tags)
+    source['note'] = 'some new note'
+    source['invite'] = set_invite()
+    source['created'] = int(datetime.datetime.now().timestamp())
+    source['modified'] = source['created']
+    # source['stats'] = [
+    #     'facts' = [
+    #         'total' = 0
+    #     ]
+    # ]
+
+    print(source)
+
+    filename = source['id'] + EXTENSION
+    with open(filename, 'w') as file:
+        yaml.dump(source, file)
 
 
-for key in user_key:
-    answer = input(f'Enter answer for item {key}:  ')
-    source[key] = answer
-
-callsign = source['callsign']
-tegs = source['tegs']
-
-source['id'] = set_id()
-source['type'] = set_type()
-source['reliability'] = 4.98
-source['tegs'] = set_tegs(tegs)
-source['note'] = 'some new note'
-source['invite'] = set_invite()
-source['created'] = datetime.datetime.now().time().strftime('%H:%M:%S')
-source['modified'] = source['created']
-source['fact'] = []
-
-
-print(source)
-filename = source['id'] + '.yaml'
-with open(filename, 'w') as file:
-    yaml.dump(source, file)
+if __name__ == "__main__":
+    main(sys.argv[1:])
