258a797c4acdab5d02191fab8fc9c71f2fda9db3
Kristina <kristinapolhutenkovatai@gmail.com>
2023-06-30 20:56:43 +0700

    @doc: add docstrings

--- tests/test_command_line_parser.py
+++ tests/test_command_line_parser.py
@@ -4,6 +4,15 @@ from src.lib.command_line_parser import parse_command_line
 
 class MyTestParseCommandLine(unittest.TestCase):
     def test_parse_command_line(self):
+        """
+        The function parse_command_line should take a list of command line arguments and return a list of tuples
+        representing the parsed options and their corresponding arguments.
+
+        The test case provides a sample list of command line arguments.
+
+        Expected behavior:
+            The function should correctly parse the command line arguments and return the expected result.
+        """
         argv = ['-c', 'Johny', '-i', 'AER24MK', '-t', 'farmer, military']
         result = parse_command_line(argv)
         expected_result = [('-c', 'Johny'), ('-i', 'AER24MK'), ('-t', 'farmer, military')]
