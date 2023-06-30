43a611d44154bc91616528691a94afabc2641155
Kristina <kristinapolhutenkovatai@gmail.com>
2023-06-30 21:09:31 +0700

    @test: add check by lenght

--- tests/test_command_line_parser.py
+++ tests/test_command_line_parser.py
@@ -1,5 +1,5 @@
 import unittest
-from src.lib.command_line_parser import parse_command_line
+from src.lib.command_line_parser import *
 
 
 class MyTestParseCommandLine(unittest.TestCase):
@@ -11,12 +11,14 @@ class MyTestParseCommandLine(unittest.TestCase):
         The test case provides a sample list of command line arguments.
 
         Expected behavior:
-            The function should correctly parse the command line arguments and return the expected result.
+            The function should correctly parse the command line arguments and return the expected list of tuples.
+            The length of the return list of the function is not less than the required options.
         """
         argv = ['-c', 'Johny', '-i', 'AER24MK', '-t', 'farmer, military']
         result = parse_command_line(argv)
         expected_result = [('-c', 'Johny'), ('-i', 'AER24MK'), ('-t', 'farmer, military')]
         self.assertEqual(result, expected_result, 'Parser should return lisf of tuples')  # add assertion here
+        self.assertTrue(len(result) >= len(REQUIRED_OPTIONS_FULL), 'Missing required arguments')
 
 
 if __name__ == '__main__':
