85918674927c6becd56aea2a3c41d5eaa03909f0
Kristina <kristinapolhutenkovatai@gmail.com>
2023-06-30 21:21:38 +0700

    @doc: add docstrings

--- src/lib/command_line_parser.py
+++ src/lib/command_line_parser.py
@@ -1,19 +1,30 @@
 import getopt
 import sys
 
+REQUIRED_OPTIONS_SHORT = {"-c", "-i"}
+REQUIRED_OPTIONS_FULL = {"--callsign", "--invited_by"}
+
 
 def parse_command_line(argv):
+    """
+    Parse command line arguments
+
+    :param argv: list[str] of command line arguments.
+
+    :return: A list of tuples representing the parsed options.
+            Each tuples contains the option and argument.
+
+    :rtype: list[tuple[str, str]]
+    """
     try:
         opts, args = getopt.getopt(argv, "c:i:t:", ["callsign=", "invited_by=", "tags="])
     except getopt.GetoptError:
         print("Invalid options or missing required arguments")
         sys.exit(2)
 
-    required_options_short = {"-c", "-i"}
-    required_options_full = {"--callsign", "--invited_by"}
     entered_options = set(opt for opt, _ in opts)
 
-    if not (required_options_short.issubset(entered_options) or required_options_full.issubset(entered_options)):
+    if not (REQUIRED_OPTIONS_SHORT.issubset(entered_options) or REQUIRED_OPTIONS_FULL.issubset(entered_options)):
         print("Not all required options entered")
         sys.exit(2)
 
