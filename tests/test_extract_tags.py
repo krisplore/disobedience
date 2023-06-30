9e294a57f42963a0488e6db4964199df058723fa
Kristina <kristinapolhutenkovatai@gmail.com>
2023-06-30 20:36:06 +0700

    @doc: add docstrings

--- tests/test_extract_tags.py
+++ tests/test_extract_tags.py
@@ -4,7 +4,13 @@ from src.lib.functions import extract_tags
 
 class TestExtractTags(unittest.TestCase):
     def test_extract_tags(self):
-        tags_string = 'tag1, tag2, tag3'
+        """
+        Check if function splits a string by comma and removes spaces at the beginning and end of each tag.
+
+        Expected behavior:
+            Should return a list of tags without spaces.
+        """
+        tags_string = 'tag1 , tag2 , tag3 '
         expected_tags = ['tag1', 'tag2', 'tag3']
         self.assertEqual(extract_tags(tags_string), expected_tags)
 
