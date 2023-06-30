169b631bea679ece3278300958717956c0518b60
Kristina <kristinapolhutenkovatai@gmail.com>
2023-06-30 20:41:28 +0700

    @doc: add docstrings

--- tests/test_generate_id.py
+++ tests/test_generate_id.py
@@ -4,6 +4,12 @@ from src.lib.functions import generate_id
 
 class TestGenerateID(unittest.TestCase):
     def test_unique_id(self):
+        """
+        Checks if function generates unique strings as IDs.
+
+        Expected behavior:
+            The function should generate unique IDs.
+        """
         source_ad = generate_id()
         source_ad2 = generate_id()
         self.assertNotEqual(source_ad, source_ad2, 'Already have this one')
