9f4adfe4e72771d470701715e903fcfbfdd53ca8
Kristina <kristinapolhutenkovatai@gmail.com>
2023-06-30 20:49:42 +0700

    @doc: add docstrings

--- tests/test_get_time.py
+++ tests/test_get_time.py
@@ -5,6 +5,13 @@ from src.lib.functions import get_time
 
 class TestGetTime(unittest.TestCase):
     def test_get_time(self):
+        """
+        Checks if the function returns a later timestamp when called multiple times.
+
+        Expected behavior:
+            The function should accurately retrieve the current time.
+            Subsequent calls to the function should return later timestamps.
+        """
         time1 = get_time()
         time.sleep(1)
         time2 = get_time()
