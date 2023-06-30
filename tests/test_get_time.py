b38cbd1d75d0e88c03e0de8f6b622a734778f440
Kristina Polkhutenkova <kristinapolhutenkovatai@gmail.com>
2023-06-30 15:12:40 +0700

    test: update import statement due to folder reorganization

--- tests/test_get_time.py
+++ tests/test_get_time.py
@@ -1,8 +1,6 @@
 import time
 import unittest
-import datetime
-from add_source_functions import get_time
-import pyperf
+from src.lib.functions import get_time
 
 
 class TestGetTime(unittest.TestCase):
