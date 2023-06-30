7229591e1f048c825c06f226639dd1779f192248
Kristina <kristinapolhutenkovatai@gmail.com>
2023-06-30 20:53:09 +0700

    @doc: add docstrings

--- tests/test_set_type.py
+++ tests/test_set_type.py
@@ -4,6 +4,12 @@ from src.lib.functions import set_type
 
 class SetTypeTestCase(unittest.TestCase):
     def test_set_type(self):
+        """
+        The function should return 1 as code for human source.
+
+        Expected behavior:
+            The function should return the value 1.
+        """
         result = set_type()
         self.assertEqual(result, 1)
 
