f0588e11fcec890c2a4e2d41486f02f06130bfe8
Kristina Polkhutenkova <kristinapolhutenkovatai@gmail.com>
2023-06-30 15:24:22 +0700

    remove: delete files (update pot, add_source_main, pet_project.pot/po/mo) - no longer needed

--- update pot file.sh
+++
@@ -1,4 +0,0 @@
- #!/bin/env sh
-
-pygettext3 -d messages -o locales/en_US/LC_MESSAGES/messages.pot add_source_main.py
-pygettext3 -d messages -o locales/ru_RU/LC_MESSAGES/messages.pot add_source_main.py
