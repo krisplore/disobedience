79f71e7c287018df27d95bc478d76d5ec2ac34c9
Kristina Polkhutenkova <kristinapolhutenkovatai@gmail.com>
2023-06-30 15:14:07 +0700

    test: add new test case

--- tests/test_generate_invite.py
+++ tests/test_generate_invite.py
@@ -2,9 +2,13 @@ import unittest
 from src.lib.functions import *
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class MyTestGenerateInvite(unittest.TestCase):
+    def test_generate_invite(self):
+        invite = generate_invite()
+        for token in invite:
+            self.assertFalse(token.isspace(), 'Invite should not be a whitespace character')
+            self.assertEqual(INVITE_LENGTH, len(token), f'The length of the invite must be = {INVITE_LENGTH}')
+        self.assertEqual(len(invite), len(set(invite)), 'Invites are not unique')
 
 
 if __name__ == '__main__':
