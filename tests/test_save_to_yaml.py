6affcf57b3cf75b19ce8c5e1d24610063da28df7
Kristina <kristinapolhutenkovatai@gmail.com>
2023-07-05 00:09:07 +0700

    @test: create test for function save_to_yaml

---
+++ tests/test_save_to_yaml.py
@@ -0,0 +1,30 @@
+import unittest
+from src.lib.work_with_yaml import *
+import os
+
+
+class MyTestSaveToYAML(unittest.TestCase):
+    def test_save_to_yaml(self):
+        """
+        Test saving a dictionary to a YAML file.
+
+        It verifies that the file is created and exists.
+
+        :return: None
+        """
+        source = {
+            'test_key1': 'value1',
+            'test_key2': 'value2',
+            'test_key3': 'value3'
+        }
+        filename = 'test_file.yaml'
+
+        save_to_yaml(source, filename)
+
+        self.assertTrue(os.path.exists(filename))
+
+        os.remove(filename)
+
+
+if __name__ == '__main__':
+    unittest.main()
