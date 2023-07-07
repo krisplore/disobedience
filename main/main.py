4a6b760cedc571e5451e36c5792eaec77c8d652a
Kristina <kristinapolhutenkovatai@gmail.com>
2023-07-07 16:02:53 +0700

    @chore: create main function

---
+++ main/main.py
@@ -0,0 +1,21 @@
+import sys
+from lib.input.getopt_entity_action import getopt_entity_action
+from source.add import add as source_add
+
+
+def main():
+    args_entity_action = getopt_entity_action(sys.argv[1:])
+    entity = args_entity_action[0]
+    action = args_entity_action[1]
+    print(entity, action)
+
+    match entity:
+        case 'source':
+            print('check')
+            source_add()
+        case _:
+            print('no')
+
+
+if __name__ == "__main__":
+    main()
--- src/main.py
+++
@@ -1,5 +0,0 @@
-import sys
-from lib.input.getopt_entity_action import getopt_entity_action
-
-args_entity_action = getopt_entity_action(sys.argv[1:])
-
