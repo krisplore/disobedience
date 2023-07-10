75d50cb634a2d4ed878833d385f73fd3fda164b2
Kristina <kristinapolhutenkovatai@gmail.com>
2023-07-10 15:01:58 +0700

    @doc: add docstrings

--- intel/definitions.py
+++ intel/definitions.py
@@ -1,3 +1,16 @@
+"""
+Module to define constants and related variables.
+
+This module contains definitions of constants and related variables.
+
+Constants:
+    PATH_BASE (str): The base path of the module, obtained from the parent directory of the current file.
+    SOURCE_SCHEMA_VERSION: The version number of the source schema.
+
+Note:
+    - Modifying the values of these constants is not recommended.
+"""
+
 from pathlib import Path
 
 PATH_BASE: str = Path(__file__).parent.parent.__str__()
