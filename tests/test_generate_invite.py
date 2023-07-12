0775c6ef35633f115e73496253ba34edbb96d9a2
Kristina <kristinapolhutenkovatai@gmail.com>
2023-07-12 23:35:37 +0700

    #docs #test add docstrings for module and class test for function generate_invite

--- tests/test_generate_invite.py
+++ tests/test_generate_invite.py
@@ -1,11 +1,31 @@
+"""
+Module containing test cases for the 'generate_invite' function.
+
+This module provides test cases to verify the behavior of the 'generate_invite'
+function in generating unique invites.
+
+Test cases:
+- MyTestGenerateInvite: Test case for generating unique invites.
+"""
+
 import unittest
 from intel.source.functions import *
 
 
 class MyTestGenerateInvite(unittest.TestCase):
+    """
+    Test case for generating unique invites using the 'generate_invite' function.
+
+    This test case verifies that the 'generate_invite' function generates unique
+    invites. It also checks that the generated invites do not contain whitespace
+    characters and have a length equal to INVITE_LENGTH.
+
+    Methods:
+        test_generate_invite: Test if function generates unique invites.
+    """
     def test_generate_invite(self):
         """
-        Checks if the function generate unique invites
+        Checks if the function generate unique invites.
 
         Expected behavior:
             The function should generate unique invites.
