e64da29eae8b9e22aee9cf014a6e1a83aaac1607
Kristina <kristinapolhutenkovatai@gmail.com>
2023-07-14 00:12:16 +0700

    #opt move validate function in separate file

--- intel/source/functions.py
+++ intel/source/functions.py
@@ -122,35 +122,3 @@ def print_dictionary(dictionary):
 
     for key, value in dictionary.items():
         print(f'{key}: {value}')
-
-
-def validate(raw_source: dict) -> dict:
-    """
-    Check if all required fields are present in the data dictionary and have non-empty values.
-
-    :param raw_source: A dictionary representing the questionnaire fields and values.
-    :type raw_source: dict
-
-    :return: A dictionary with the result of the checks.
-    :rtype: dict
-    """
-
-    result = {
-        'status': True,
-        'errors': {}
-    }
-
-    for field in SOURCE_FIELDS_REQUIRED:
-        field_value = raw_source.get(field)
-        result['errors'][field] = []
-        if field_value is None:
-            result['errors'][field].append('empty')
-            result['status'] = False
-        elif field_value.strip() == '':
-            result['errors'][field] = ['empty']
-            result['status'] = False
-        elif field not in raw_source:
-            result['errors'][field] = ['missing']
-            result['status'] = False
-
-    return result
