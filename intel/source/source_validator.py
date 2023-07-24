57894089919b33663745d5ccdcf4ca4215125b3e
Kristina <kristinapolhutenkovatai@gmail.com>
2023-07-24 15:57:02 +0700

    #docs: add docstring to source_validator.py

--- intel/source/source_validator.py
+++ intel/source/source_validator.py
@@ -1,14 +1,25 @@
-from intel.definitions import SOURCE_FIELDS_REQUIRED
+"""
+The 'source_validator' module provides utility function for data validation and verification.
+
+This module contains the following function:
+    - validate: Check if all required fields are present in the data dictionary and have non-empty values.
+"""
 
 
-def validate(raw_source: dict) -> dict:
+def validate(raw_source: dict, model) -> dict:
     """
     Check if all required fields are present in the data dictionary and have non-empty values.
+
+    :param model: A dictionary containing validation rules for each field.
+    :type model: dict
 
     :param raw_source: A dictionary representing the questionnaire fields and values.
     :type raw_source: dict
 
-    :return: A dictionary with the result of the checks.
+    :return: A dictionary with the result of the checks. It contains the 'status' key, which indicates
+             whether all required fields are present and have non-empty values (True if valid, False otherwise),
+             and the 'errors' key, which contains a list of error messages if any validation rules are violated.
+             If no errors are found, the 'errors' list will be empty.
     :rtype: dict
     """
 
