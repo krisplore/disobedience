2d7b72b013a6be899a15d177479d39b6e98a35c0
Kristina <kristinapolhutenkovatai@gmail.com>
2023-07-25 17:21:36 +0700

    #fix source_validator → validator

--- intel/source/add.py
+++ intel/source/add.py
@@ -9,7 +9,7 @@ from intel.source.functions import create_source_stub, print_dictionary
 from intel.source.input_file import parse_filename
 from intel.source.input_method import parse_method_input
 from intel.source.input_options import parse_options
-from intel.source.validate.source_validator import validate
+from intel.source.validate.validator import validate
 from intel.source.yaml_functions import read_from_yaml
 from intel.source.yaml_functions import save_to_yaml
 from intel.translation import start_translating
