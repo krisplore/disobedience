79771c2d2396d327f09db2e03caa2084cfe15548
Kristina <kristinapolhutenkovatai@gmail.com>
2023-08-03 22:00:59 +0700

    #ref function edit

--- intel/source/edit.py
+++ intel/source/edit.py
@@ -2,11 +2,11 @@
 
 import getopt
 import sys
-import yaml
 
-from intel.definitions import PATH_TO_SOURCE_MODEL, SOURCE_EXTENSION_YAML
-from intel.source.functions import extract_items_from_list, print_dictionary
-from intel.source.my_yaml import read
+from intel.definitions import PATH_TO_SOURCE_MODEL, SOURCE_EXTENSION_YAML, PATH_TO_STORAGE
+from intel.source.edit_parser import parse_edit_options
+from intel.source.functions import print_dictionary
+from intel.source.my_yaml import read, write
 from intel.source.validate.validator import validate
 
 
@@ -20,36 +20,24 @@ def edit():
         print(str(err))
         sys.exit(2)
 
-    for opt, arg in opts:
-        if opt in '--where.id':
-            user_id = arg
-        elif opt in '--new.tags':
-            raw_tags = arg
-
-    arguments = {
-        'id': user_id,
-        'tags': extract_items_from_list(raw_tags),
-    }
+    arguments = parse_edit_options(sys.argv[3:])
 
     filename = arguments['id']
 
-    with open(filename + SOURCE_EXTENSION_YAML, 'r', encoding='utf-8') as f:
-        file: dict = yaml.safe_load(f)
+    source = read(PATH_TO_STORAGE + filename + SOURCE_EXTENSION_YAML)
 
-    file['tags'] = arguments['tags']
+    source.update(arguments)
 
-    result = validate(file, read(PATH_TO_SOURCE_MODEL + SOURCE_EXTENSION_YAML))
+    model = read(PATH_TO_SOURCE_MODEL + SOURCE_EXTENSION_YAML)
+
+    result = validate(source, model)
 
     if not result['status']:
         print(result)
         sys.exit(2)
 
     else:
-        with open(filename + SOURCE_EXTENSION_YAML, 'w', encoding='utf-8') as new_file:
-            yaml.dump(file, new_file)
-        print_dictionary(result, read(PATH_TO_SOURCE_MODEL + SOURCE_EXTENSION_YAML))
-        print_dictionary(file, read(PATH_TO_SOURCE_MODEL + SOURCE_EXTENSION_YAML))
+        write(source, filename)
 
-
-if __name__ == "__main__":
-    edit()
+        print_dictionary(result, model)
+        print_dictionary(source, model)
