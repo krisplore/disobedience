3c5edaf286ac96b920b23d968eee0d7d7315c518
Kristina <kristinapolhutenkovatai@gmail.com>
2023-08-04 20:08:15 +0700

    #test edit with new functionality of function test_items_from_list

--- tests/test_extract_tags.py
+++ tests/test_extract_tags.py
@@ -1,39 +1,44 @@
 """
-Module containing test cases for the 'extract_tags' function.
+Module containing test cases for the 'test_items_from_list' function.
 
-This module provides test cases to verify the behavior of the 'extract_tags' function
+This module provides test cases to verify the behavior of the 'test_items_from_list' function
 in extracting tags from a string.
 
 Test cases:
-- TestExtractTags: Test case for extracting tags from a string.
+- TestCaseExtractItemsFromList: Test case for extracting tags from a string.
 """
 
 import unittest
+import yaml
 from intel.source.functions import extract_items_from_list
 
 
-class TestCaseExtractTags(unittest.TestCase):
+class TestCaseExtractItemsFromList(unittest.TestCase):
     """
-    Test case for extracting tags from a string using the 'extract_tags' function.
+    Test case for extracting tags from a string using the 'test_items_from_list' function.
 
-    This test case verifies that the 'extract_tags' function correctly splits a string
+    This test case verifies that the 'test_items_from_list' function correctly splits a string
     by commas and removes spaces at the beginning and end of each tag. It checks that
-    the function returns a list of tags without spaces.
+    the function returns a dict of list of tags without spaces.
 
     Methods:
-        test_extract_tags: Test extracting tags from a string.
+        test_items_from_list: Test extracting tags from a string.
     """
-    def test_extract_tags(self):
+    def test_items_from_list(self):
         """
         Test extracting tags from a string.
 
         Expected behavior:
             The function should split the string by comma and remove spaces at the beginning
-            and end of each tag. It should return a list of tags without spaces.
+            and end of each tag. It should return a dict with list of tags without spaces.
         """
-        tags_string = 'tag1 , tag2 , tag3 '
-        expected_tags = ['tag1', 'tag2', 'tag3']
-        self.assertEqual(extract_items_from_list(tags_string), expected_tags)
+        with open('models/source.yaml', 'r', encoding='utf-8') as file:
+            model: dict = yaml.safe_load(file)
+
+        new_values = {'tags': '  tag1 , tag2 , tag3 '}
+        expected_values = {'tags': ['tag1', 'tag2', 'tag3']}
+        extract_items_from_list(new_values, model)
+        self.assertEqual(new_values, expected_values)
 
 
 if __name__ == '__main__':
