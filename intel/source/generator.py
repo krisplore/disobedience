99e7caf0fde2108e588a1ee78865656bf52ad55d
Kristina <kristinapolhutenkovatai@gmail.com>
2023-08-04 18:37:17 +0700

    #docs #fix docstrings

--- intel/source/generator.py
+++ intel/source/generator.py
@@ -3,20 +3,20 @@ The 'options_generator' module provides a function to generate options for searc
 
 This module contains the following function:
     - generate_options: Extracts searchable and editable options from the given model and returns them
-      as dictionaries with field names as keys and corresponding short and long options as values.
+      as list with options as values.
 """
 
 
 def generate_options(model):
     """
     The function scans the Model dictionary for the presence of the can_be_searched
-    and can_be_edited fields and saves to the appropriate dictionaries.
+    and can_be_edited fields and saves to the appropriate list.
 
     :param model: A dictionary representing the model containing field properties and options.
     :type: dict
 
-    :return: two dictionaries containing searchable and editable options respectively.
-    :rtype: tuple if dictionaries
+    :return: List of command line options.
+    :rtype: List
     """
 
     searchable_options = {}
