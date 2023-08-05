269c7784f5e77dafd9e31e7af020d27c3ddc18cc
Kristina <kristinapolhutenkovatai@gmail.com>
2023-08-05 15:40:46 +0700

    #chore add logging to length module

--- intel/source/validate/length.py
+++ intel/source/validate/length.py
@@ -7,6 +7,16 @@ This module contains the following function:
 
 Note: This module is designed for use with Python 3.x.
 """
+import logging
+
+py_logger8 = logging.getLogger(__name__)
+py_logger8.setLevel(logging.INFO)
+
+py_handler = logging.FileHandler(f"logs/{__name__}.log", mode='w')
+py_formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
+
+py_handler.setFormatter(py_formatter)
+py_logger8.addHandler(py_handler)
 
 
 def validate_length(raw_source: dict, model: dict, result: dict):
@@ -27,6 +37,7 @@ def validate_length(raw_source: dict, model: dict, result: dict):
     :return: The modified 'result' dictionary after checking the length of specified fields.
     :rtype: dict
     """
+    py_logger8.info("validate_length function was called")
 
     for key, rules in model.items():
         if 'length' in rules:
@@ -34,12 +45,16 @@ def validate_length(raw_source: dict, model: dict, result: dict):
                 value = raw_source[key]
                 if not isinstance(value, (str, list)):
                     if 'length' in rules and 'min' in rules['length'] and len(value) < rules['length']['min']:
+                        py_logger8.warning("Invalid length for '%s'. Length must be between %s and %s "
+                                           "characters.", key, rules["length"]["min"], rules["length"]["max"])
                         result['status'] = False
                         result['errors'].append(f'The length of the {key} must be between '
                                                 f'{rules["length"]["min"]} and '
                                                 f'{rules["length"]["max"]} characters')
 
                     if 'length' in rules and 'max' in rules['length'] and len(value) > rules['length']['max']:
+                        py_logger8.warning("Invalid length for '%s'. Length must be between %s and %s "
+                                           "characters.", key, rules["length"]["min"], rules["length"]["max"])
                         result['status'] = False
                         result['errors'].append(f'The length of the {key} must be between '
                                                 f'{rules["length"]["min"]} and '
