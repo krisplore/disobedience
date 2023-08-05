8362e49de7e3cebe88b62aa7a37ef4978419cabe
Kristina <kristinapolhutenkovatai@gmail.com>
2023-08-05 15:12:20 +0700

    #chore add logging to functions module

--- intel/source/functions.py
+++ intel/source/functions.py
@@ -3,6 +3,7 @@ This module provides functions for determining characteristics of a source.
 """
 
 import datetime
+import logging
 import secrets
 import string
 import uuid
@@ -10,6 +11,15 @@ from babel import default_locale, UnknownLocaleError
 from babel.dates import format_datetime
 from intel.definitions import SOURCE_SCHEMA_VERSION, PATH_TO_SOURCE_MODEL, SOURCE_EXTENSION_YAML
 from intel.source.my_yaml import read
+
+py_logger5 = logging.getLogger(__name__)
+py_logger5.setLevel(logging.INFO)
+
+py_handler = logging.FileHandler(f"logs/{__name__}.log", mode='w')
+py_formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
+
+py_handler.setFormatter(py_formatter)
+py_logger5.addHandler(py_handler)
 
 
 def extract_items_from_list(new_values, model):
@@ -20,11 +30,17 @@ def extract_items_from_list(new_values, model):
     :param model: The dictionary representing the model containing field properties and options.
     :return: None
     """
+    py_logger5.info("extract_items_from_list function was called")
+
     for field, value in new_values.items():
         field_type = model.get(field, {}).get('type', '')
         if 'list string separator comma' in field_type:
+            py_logger5.info(f"type 'list string separator comma' in {field_type}")
             extracted_items = [item.strip() for item in value.split(',') if item.strip()]
             new_values[field] = extracted_items
+            py_logger5.info("items were extract")
+
+    return new_values
 
 
 def generate_id():
@@ -34,7 +50,7 @@ def generate_id():
     :return: identifier for source.
     :rtype: str.
     """
-
+    py_logger5.info("generate_id function was called")
     return str(uuid.uuid4())
 
 
@@ -45,7 +61,7 @@ def set_type():
     :return: code for type of source.
     :rtype: int.
     """
-
+    py_logger5.info("set_type function was called")
     return 1
 
 
@@ -56,11 +72,20 @@ def generate_invite(model):
     :return: list of invites, each of invite being a string.
     :rtype: list.
     """
+    py_logger5.info("generate_invite function was called")
+
     alphabet: str = string.ascii_uppercase + string.digits
+    py_logger5.info("Alphabet was written from the string module")
     invite = []
+
     charset_blacklist = model.get('invite', {}).get('charset', {}).get('blacklist', ['B8CDO0QIJ1GS5'])
+    py_logger5.info("Blacklist was written from the model")
+
     invite_item_length = model.get('invite', {}).get('item', {}).get('length', 7)
+    py_logger5.info("Length of invite was written from the model")
+
     invite_length = model.get('invite', {}).get('length', 2)
+    py_logger5.info("List length was written from the model")
 
     characters: list[str] = [c for c in alphabet if c not in charset_blacklist]
 
@@ -77,6 +102,7 @@ def get_time():
     :return: time of creation source data.
     :rtype: int
     """
+    py_logger5.info("get_time function was called")
 
     return int(datetime.datetime.now().timestamp())
 
@@ -88,8 +114,10 @@ def create_stub():
     :return: The created source dictionary.
     :rtype: Dict[str, Any]
     """
+    py_logger5.info("create_stub function was called")
 
     time_of_creation = get_time()
+    py_logger5.info("Time of creation was set")
 
     source = {
         '_source_schema_version': SOURCE_SCHEMA_VERSION,
@@ -122,6 +150,8 @@ def get_system_locale():
     :return: system locale of the user as a string in the format 'language_region'.
     :rtype:str
     """
+    py_logger5.info("get_system_locale function was called")
+
     user_locale: str = default_locale()
 
     return user_locale or 'en_US'
@@ -136,13 +166,16 @@ def convert_date(value, user_locale):
     :return: The formatted value as a string.
     :rtype: str.
     """
+    py_logger5.info("convert_date function was called")
 
     if isinstance(value, int):
         try:
             d_t = datetime.datetime.fromtimestamp(value)
             formatted_date = format_datetime(d_t, format='short', locale=user_locale)
+            py_logger5.info(f"Date {value} formatted using locale {user_locale}")
             return formatted_date
         except UnknownLocaleError as error:
+            py_logger5.error(f"Error formatting date: {error}")
             print(f"Error formatting date: {error}")
 
     return str(value)
@@ -154,13 +187,17 @@ def print_dictionary(dictionary, model):
 
     :param: dictionary (Dict[str, Any]): The dictionary.
     """
+    py_logger5.info("print_dictionary function was called")
 
     sorted_items = sorted(dictionary.items())
     for key, value in sorted_items:
         if key in model and model[key].get('type') == 'date':
+            py_logger5.info(f"Converting date value for key '{key}' to human-readable format.")
             value = convert_date(value, get_system_locale())
         if not value:
+            py_logger5.warning(f"The value for key '{key}' is empty.")
             value = 'the field is empty'
+
         print(f'{key}: {value}')
 
 
@@ -171,4 +208,6 @@ def replace_hyphen_with_underscore(input_string):
     :param input_string: The input string where hyphens need to be replaced.
     :return: The modified string with hyphens replaced by underscores.
     """
+    py_logger5.info("replace_hyphen_with_underscore function was called")
+
     return input_string.replace('-', '_')
