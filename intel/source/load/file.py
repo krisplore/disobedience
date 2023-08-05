d7014a3128954769dd1a759f113cba20e3c554e9
Kristina <kristinapolhutenkovatai@gmail.com>
2023-08-05 16:00:54 +0700

    #chore add logging to file module

--- intel/source/load/file.py
+++ intel/source/load/file.py
@@ -3,11 +3,21 @@ This module provides a function for extracting the filename from command line ar
 """
 
 import getopt
+import logging
 import sys
 
 from intel.translation import start_translating
 
 _ = start_translating()
+
+py_logger12 = logging.getLogger(__name__)
+py_logger12.setLevel(logging.INFO)
+
+py_handler = logging.FileHandler(f"logs/{__name__}.log", mode='w')
+py_formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
+
+py_handler.setFormatter(py_formatter)
+py_logger12.addHandler(py_handler)
 
 
 def parse_filename(argv):
@@ -19,15 +29,20 @@ def parse_filename(argv):
     :return: The filename extracted from the command line arguments.
     :rtype: str
     """
+    py_logger12.info("parse_filename function was called")
 
     try:
         opts = getopt.getopt(argv, "f:", ["filename="])[0]
     except getopt.GetoptError:
+        py_logger12.error("Error parsing filename option: %s", e)
         print(_("Invalid option or missing required argument"))
         sys.exit(2)
 
     if opts:
+        py_logger12.info("Filename provided")
         filename = opts[0][1]
         return filename
+
+    py_logger12.warning("No filename provided")
     print(_("No filename provided"))  # if admin did not enter option at all
     sys.exit(2)
