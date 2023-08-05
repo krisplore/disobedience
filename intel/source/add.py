b97a53b6a0d2e8ba9a1cb777b161f3d15ccd2fe0
Kristina <kristinapolhutenkovatai@gmail.com>
2023-08-05 13:16:12 +0700

    #chore add logging to add

--- intel/source/add.py
+++ intel/source/add.py
@@ -15,6 +15,15 @@ from intel.translation import start_translating
 
 _ = start_translating()
 
+py_logger = logging.getLogger(__name__)
+py_logger.setLevel(logging.INFO)
+
+py_handler = logging.FileHandler(f"{__name__}.log", mode='w')
+py_formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
+
+py_handler.setFormatter(py_formatter)
+py_logger.addHandler(py_handler)
+
 
 def add():
     """
@@ -22,30 +31,44 @@ def add():
     It prompts the user for required information, generates relevant data,
     and saves it to a file.
     """
-
+    py_logger.info("Add function started")
     print(_("Language"))
 
     source = create_stub()
+    py_logger.info("The stub for the source was created")
 
     method_input = parse_method_input()
+    py_logger.info("Input method defined")
 
     match method_input:
         case 'file':
+            py_logger.info("Case input method - file defined")
             raw_source = read(parse_filename(sys.argv[4:]))
         case 'opt':
+            py_logger.info("Case input method - options defined")
             raw_source = parse_options(sys.argv[4:])
         case _:
+            py_logger.error("Case input method not defined")
             print(_('Method does not exist'))
             sys.exit(2)
 
     result = validate(raw_source, read(PATH_TO_SOURCE_MODEL + SOURCE_EXTENSION_YAML))
 
     if not result['status']:
+        py_logger.error("File validation failed")
         print(result)
         sys.exit(2)
     else:
+        py_logger.info("File validation completed successfully")
+
         print_dictionary(result, read(PATH_TO_SOURCE_MODEL + SOURCE_EXTENSION_YAML))
+        py_logger.info("The result was printed")
+
         source.update(raw_source)
+        py_logger.info("The raw source was merged into the stub source")
 
         write(source, source['id'])
+        py_logger.info("The source was written to a file")
+
         print_dictionary(source, read(PATH_TO_SOURCE_MODEL + SOURCE_EXTENSION_YAML))
+        py_logger.info("The source was printed")
