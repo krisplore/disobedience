25980e9e94509f5dd78c6902a202e3f3c4e73162
Kristina <kristinapolhutenkovatai@gmail.com>
2023-08-05 15:51:32 +0700

    #chore add logging to my_yaml module

--- intel/source/my_yaml.py
+++ intel/source/my_yaml.py
@@ -5,11 +5,21 @@ The module includes the following functions:
 - read: Reads data from a YAML file and returns the parsed content.
 - write: Saves a dictionary to a YAML file.
 """
+import logging
 
 import yaml
 from intel.definitions import SOURCE_EXTENSION_YAML, PATH_BASE
 
 PATH_TO_STORAGE: str = PATH_BASE + '/data/source/'
+
+py_logger10 = logging.getLogger(__name__)
+py_logger10.setLevel(logging.INFO)
+
+py_handler = logging.FileHandler(f"logs/{__name__}.log", mode='w')
+py_formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
+
+py_handler.setFormatter(py_formatter)
+py_logger10.addHandler(py_handler)
 
 
 def read(filename):
@@ -22,11 +32,19 @@ def read(filename):
     :return: A dictionary representing the parsed content from the YAML file.
     :rtype: dict
     """
+    py_logger10.info("read function was called")
 
-    with open(filename, 'r', encoding='utf-8') as file:
-        file_parsed: dict = yaml.safe_load(file)
-
-        return file_parsed
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            file_parsed: dict = yaml.safe_load(file)
+            py_logger10.info("File '%s' successfully read.", filename)
+            return file_parsed
+    except FileNotFoundError as e:
+        py_logger10.error("File not found: %s", filename)
+        raise e
+    except yaml.YAMLError as e:
+        py_logger10.error("Error parsing the YAML file '%s': %s", filename, e)
+        raise e
 
 
 def write(dictionary, filename):
@@ -41,6 +59,12 @@ def write(dictionary, filename):
 
     :return: None
     """
+    py_logger10.info("write function was called")
 
-    with open(PATH_TO_STORAGE + filename + SOURCE_EXTENSION_YAML, 'w', encoding='utf-8') as file:
-        yaml.dump(dictionary, file)
+    try:
+        with open(PATH_TO_STORAGE + filename + SOURCE_EXTENSION_YAML, 'w', encoding='utf-8') as file:
+            yaml.dump(dictionary, file)
+        py_logger10.info("Dictionary successfully written to file '%s'.", filename + SOURCE_EXTENSION_YAML)
+    except Exception as e:
+        py_logger10.error("Error writing dictionary to file '%s': %s", filename + SOURCE_EXTENSION_YAML, e)
+        raise e
