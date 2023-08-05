439c2d227e24c1c8e9c898236599ccca06300b93
Kristina <kristinapolhutenkovatai@gmail.com>
2023-08-05 10:03:10 +0700

    #test create for replace_hyphen_with_underscore function

---
+++ tests/test_replace_hyphen_with_underscore.py
@@ -0,0 +1,27 @@
+"""
+The module provides a test case to test the function replace_hyphen_with_underscore.
+"""
+import unittest
+
+from intel.source.functions import replace_hyphen_with_underscore
+
+
+class TestCaseSwitchFunction(unittest.TestCase):
+    """
+    The test case provides one case for the function to work correctly.
+    Expected result: The function will return a string with dashes replaced with underscores.
+    """
+
+    def test_replace_hyphen_with_underscore(self):
+        """
+        The test gives a line for the switch. The function should return the expected result.
+        """
+
+        test_data = 'invited-by-John'
+        expected_result = 'invited_by_John'
+        result = replace_hyphen_with_underscore(test_data)
+        self.assertEqual(result, expected_result, 'The function should return the expected result.')
+
+
+if __name__ == '__main__':
+    unittest.main()
