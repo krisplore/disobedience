70631fc2ae3f3d6a01e2e887b2efee85aeb34046
Kristina <kristinapolhutenkovatai@gmail.com>
2023-08-05 16:00:48 +0700

    #chore add logging to method module

--- intel/source/load/method.py
+++ intel/source/load/method.py
@@ -1,8 +1,17 @@
 """
 Module provides a function for parsing input argument and retrieving the data input method.
 """
-
+import logging
 import sys
+
+py_logger13 = logging.getLogger(__name__)
+py_logger13.setLevel(logging.INFO)
+
+py_handler = logging.FileHandler(f"logs/{__name__}.log", mode='w')
+py_formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
+
+py_handler.setFormatter(py_formatter)
+py_logger13.addHandler(py_handler)
 
 
 def parse_method_input():
@@ -12,6 +21,7 @@ def parse_method_input():
     :return: The data input method.
     :rtype: str.
     """
+    py_logger13.info("parse_method_input function was called")
 
     way: str = sys.argv[3]
 
