39cc4f6ca3d0d582723321debe7f4c0f9f1725d8
Kristina <kristinapolhutenkovatai@gmail.com>
2023-08-07 19:43:43 +0700

    #fix function generate invite after add fields min max to model invite

--- intel/source/functions.py
+++ intel/source/functions.py
@@ -77,13 +77,13 @@ def generate_invite(model):
     invite_length = model.get('invite', {}).get('length', {}).get('min', 2)
     logger.info("List length was written from the model")
 
-    invite_item_length = model.get('invite', {}).get('item', {}).get('length', 7)
+    invite_item_max_length = model.get('invite', {}).get('item', {}).get('length', {}).get('max', 7)
     logger.info("Length of invite was written from the model")
 
     characters: list[str] = [c for c in alphabet if c not in charset_blacklist]
 
     for _ in range(invite_length):
-        token_bytes: bytes = secrets.token_bytes(invite_item_length)
+        token_bytes: bytes = secrets.token_bytes(invite_item_max_length)
         invite.append(''.join(characters[b % len(characters)] for b in token_bytes))
     return invite
 
