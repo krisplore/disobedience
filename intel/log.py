c9851ff1ab493c51a04e1ab30d4fad1e7667b3ac
Kristina <kristinapolhutenkovatai@gmail.com>
2023-08-07 19:11:22 +0700

    #opt check for folder log before write

--- intel/log.py
+++ intel/log.py
@@ -3,6 +3,9 @@ Module setup logger.
 """
 
 import logging
+import os
+
+from intel.definitions import LOG_LEVEL
 
 
 def setup_logger():
@@ -10,6 +13,8 @@ def setup_logger():
     The function sets up and returns a logger object for writing logs to the 'logs/disobedience.log' file
     :return: the created and configured logger object
     """
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
 
     handler = logging.FileHandler('logs/disobedience.log', mode='w')
     formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
