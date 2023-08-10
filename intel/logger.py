723118ba0ca4e9f2426b7c62401f02de6a19813a
Kristina <kristinapolhutenkovatai@gmail.com>
2023-08-10 22:06:58 +0700

    #fix call for path to logs from definitions

--- intel/logger.py
+++ intel/logger.py
@@ -13,9 +13,8 @@ def setup():
     The function sets up and returns a logger object for writing logs to the 'logs/disobedience.log' file
     :return: the created and configured logger object
     """
-    if not os.path.exists('logs'):
-        os.makedirs('logs')
     if not os.path.exists(PATH_TO_LOGS):
+        os.makedirs(PATH_TO_LOGS)
 
     handler = logging.FileHandler('logs/disobedience.log', mode='w')
     formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s", datefmt="%H:%M:%S")
