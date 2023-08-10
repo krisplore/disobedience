2d7ae22c2584f418460df93945719e81add39b68
Kristina <kristinapolhutenkovatai@gmail.com>
2023-08-10 21:09:05 +0700

    #fix reorganized logging messages for functions module

--- intel/source/functions.py
+++ intel/source/functions.py
@@ -26,8 +26,10 @@ def generate_id():
     :return: identifier for source.
     :rtype: str.
     """
-    logger.info("generate_id function was called")
-    return str(uuid.uuid4())
+    generated_id = str(uuid.uuid4())
+    if generated_id:
+        logger.debug("Id was generated")
+    return generated_id
 
 
 def set_type():
@@ -37,7 +39,7 @@ def set_type():
     :return: code for type of source.
     :rtype: int.
     """
-    logger.info("set_type function was called")
+
     return 1
 
 
@@ -48,26 +50,32 @@ def generate_invite(model):
     :return: list of invites, each of invite being a string.
     :rtype: list.
     """
-    logger.info("generate_invite function was called")
 
     alphabet: str = string.ascii_uppercase + string.digits
-    logger.info("Alphabet was written from the string module")
+    if alphabet:
+        logger.debug("Alphabet was written from the string module")
     invite = []
 
     charset_blacklist = model.get('invite', {}).get('charset', {}).get('blacklist', ['B8CDO0QIJ1GS5'])
-    logger.info("Blacklist was written from the model")
+    if charset_blacklist:
+        logger.debug("Blacklist was written from the model")
 
     invite_length = model.get('invite', {}).get('length', {}).get('min', 2)
-    logger.info("List length was written from the model")
+    if invite_length:
+        logger.debug("List length was written from the model")
 
     invite_item_max_length = model.get('invite', {}).get('item', {}).get('length', {}).get('max', 7)
-    logger.info("Length of invite was written from the model")
+    if invite_item_max_length:
+        logger.debug("Length of invite was written from the model")
 
     characters: list[str] = [c for c in alphabet if c not in charset_blacklist]
 
     for _ in range(invite_length):
         token_bytes: bytes = secrets.token_bytes(invite_item_max_length)
         invite.append(''.join(characters[b % len(characters)] for b in token_bytes))
+    if len(invite):
+        logger.debug("Invite was generated")
+
     return invite
 
 
@@ -78,9 +86,11 @@ def get_time():
     :return: time of creation source data.
     :rtype: int
     """
-    logger.info("get_time function was called")
+    time = int(datetime.datetime.now().timestamp())
+    if time:
+        logger.debug("time was received")
 
-    return int(datetime.datetime.now().timestamp())
+    return time
 
 
 def create_stub():
@@ -90,10 +100,10 @@ def create_stub():
     :return: The created source dictionary.
     :rtype: Dict[str, Any]
     """
-    logger.info("create_stub function was called")
 
     time_of_creation = get_time()
-    logger.info("Time of creation was set")
+    if time_of_creation:
+        logger.debug("Time of creation was set")
 
     source = {
         '_schema_version': SOURCE_SCHEMA_VERSION,
@@ -115,6 +125,8 @@ def create_stub():
             }
         }
     }
+    if source:
+        logger.debug("The source stub was created")
 
     return source
 
@@ -126,11 +138,13 @@ def get_system_locale():
     :return: system locale of the user as a string in the format 'language_region'.
     :rtype:str
     """
-    logger.info("get_system_locale function was called")
 
     user_locale: str = default_locale()
-
-    return user_locale or 'en_US'
+    if user_locale:
+        logger.debug("system locale was found")
+        return user_locale
+    logger.info("en_US locale was set")
+    return 'en_US'
 
 
 def convert_date(value, user_locale):
@@ -142,13 +156,12 @@ def convert_date(value, user_locale):
     :return: The formatted value as a string.
     :rtype: str.
     """
-    logger.info("convert_date function was called")
 
     if isinstance(value, int):
         try:
             d_t = datetime.datetime.fromtimestamp(value)
             formatted_date = format_datetime(d_t, format='short', locale=user_locale)
-            logger.info("Date %s formatted using locale %s", value, user_locale)
+            logger.debug("Date %s formatted", value)
             return formatted_date
         except UnknownLocaleError as error:
             logger.error("Error formatting date: %s", error)
@@ -163,19 +176,18 @@ def print_dictionary(dictionary, model):
 
     :param: dictionary (Dict[str, Any]): The dictionary.
     """
-    logger.info("print_dictionary function was called")
 
     dictionary = write(dictionary, model)
     sorted_items = sorted(dictionary.items())
     for key, value in sorted_items:
         if key in model and model[key].get('type') == 'date':
-            logger.info("Converting date value for key '%s' to human-readable format.", key)
             value = convert_date(value, get_system_locale())
         if not value:
-            logger.warning("The value for key '%s' is empty.", key)
+            logger.debug("The value for key '%s' is empty.", key)
             value = 'the field is empty'
 
         print(f'{key}: {value}')
+        logger.debug("The result was printed")
 
 
 def sync_name(input_string):
@@ -185,6 +197,8 @@ def sync_name(input_string):
     :param input_string: The input string where hyphens need to be replaced.
     :return: The modified string with hyphens replaced by underscores.
     """
-    logger.info("synch_name function was called")
+    output_string = input_string.replace('-', '_')
+    if output_string:
+        logger.debug("The %s has been synchronized with the model", input_string)
 
-    return input_string.replace('-', '_')
+    return output_string
