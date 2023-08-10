71f0812a15988bdaeb0e2a19818db369a6f8fba1
Kristina <kristinapolhutenkovatai@gmail.com>
2023-08-10 22:07:20 +0700

    #fix call for name project from definitions

--- intel/logger.py
+++ intel/logger.py
@@ -5,7 +5,7 @@ Module setup logger.
 import logging
 import os
 
-from intel.definitions import LOG_LEVEL
+from intel.definitions import LOG_LEVEL, NAME_PROJECT, PATH_TO_LOGS
 
 
 def setup():
@@ -16,11 +16,11 @@ def setup():
     if not os.path.exists(PATH_TO_LOGS):
         os.makedirs(PATH_TO_LOGS)
 
-    handler = logging.FileHandler('logs/disobedience.log', mode='w')
+    handler = logging.FileHandler(PATH_TO_LOGS + NAME_PROJECT + '.log', mode='w')
     formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s", datefmt="%H:%M:%S")
     handler.setFormatter(formatter)
 
-    logger = logging.getLogger('disobedience')
+    logger = logging.getLogger(NAME_PROJECT)
     logger.setLevel(LOG_LEVEL)
     logger.addHandler(handler)
 
