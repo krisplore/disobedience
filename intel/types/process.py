ad1664a21fbba179f76bca45f0ec0400a8ef5076
Kristina <kristinapolhutenkovatai@gmail.com>
2023-08-11 20:05:58 +0700

    #docs: add docstrings to process module

--- intel/types/process.py
+++ intel/types/process.py
@@ -1,3 +1,10 @@
+"""
+The module provides 2 functions for reading and writing information to the screen for the user
+and to a file for the program.
+
+The functions determine the data type and sends the information to the next function.
+"""
+
 from intel.logger import setup as logger_setup
 from intel.types.list_as_string import las_read, las_write
 
@@ -5,6 +12,13 @@ logger = logger_setup()
 
 
 def read(data, model):
+    """
+    Responsible for block reading information from the screen into a file.
+
+    :param data: dictionary to be read in correct format
+    :param model: dictionary with rules for data
+    :return: correctly read information
+    """
     for field, properties in model.items():
         field_type = properties.get('type')
         if field in data:
@@ -18,6 +32,14 @@ def read(data, model):
 
 
 def write(data, model):
+    """
+    The function responsible for the block of writing information from the file
+    to the screen in a human-readable format.
+
+    :param data: dictionary to be written in correct format
+    :param model: dictionary with rules for data
+    :return: correct written information
+    """
     for field, properties in model.items():
         field_type = properties.get('type')
         if field in data:
